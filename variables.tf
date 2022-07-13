0e78c72a0460825382904a9dddf594adef673456
Anstartla <srilalithamadupalli20@gmail.com>
2022-07-13 10:07:03 +0000

    testt

--- variables.tf
+++ variables.tf
@@ -16,37 +16,13 @@ variable "ami_id" {
     description = "image id of os to be installed"
     type = string
 }
-variable "ami_id_adminportal" {
-}
-variable "ami_id_agentportal" {
-}
-variable "ami_id_dashboardportal" {
-}
-variable "ami_id_reportingportal" {
-}
 variable "ami_id_apiserver" {
-}
-variable "min_size_portals" {
-}
-variable "max_size_portals" {
 }
 variable "min_size_apiserver" {
 }
 variable "max_size_apiserver" {
 }
-variable "portals_instances_names" {
-  description = "list of names for the instances"
-  type = list(string) 
-}
-variable "portals_instances_names1" {
-  description = "list of names for the instances"
-  type = list(string) 
-}
 variable "instance_type_4_16" {
-  description = "Instance type to use for EC2 Instance"
-  type        = string
-}
-variable "instance_type_8_32" {
   description = "Instance type to use for EC2 Instance"
   type        = string
 }
@@ -67,25 +43,7 @@ variable "key_pair_name_ec2" {}
 variable "name_prefix" {
   description = "name for the resources to be created"
 }
-variable "db_names" {
-  type = list(string)
-}
-variable "num_alb_portals" {
-  type = number 
-}
-variable "num_alb" {
-  type = number 
-}
-variable "apiserver_domain_names" {
-}
-variable "target_ports_portals" {
-  description = "A list of listener blocks"
-}
 variable "target_ports_apiserver" {}
-variable "db_instance_type" {
-  description = "instance type for db"
-  type = string
-}
 variable "tags" {
   description = "A mapping of tags to assign to the resource"
   type        = map(string)
